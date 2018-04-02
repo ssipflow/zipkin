67b003bb1717b1d43c255bd65b7ae56b8d2f9865
Neslon Kim <ssipflow@gmail.com>
2018-04-02 17:13:30 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 #curl -sSL https://zipkin.io/quickstart.sh | bash -s
-docker build . -t hubtea/zipkin
-docker push hubtea/zipkin
+docker build . -t ssipflow/zipkin
+docker push ssipflow/zipkin
 kubectl apply -f deployment.yaml -n development
